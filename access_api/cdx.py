150a259575e8eef41acd91a89eb363ad83617941
Andrew Jackson <Andrew.Jackson@bl.uk>
2019-04-29 15:28:25 +0100

    Allow ISO format date.

--- access_api/cdx.py
+++ access_api/cdx.py
@@ -7,6 +7,7 @@ import requests
 from collections import OrderedDict
 
 WAYBACK_TS_FORMAT = '%Y%m%d%H%M%S'
+ISO_TS_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
 CDX_SERVER = os.environ.get('CDX_SERVER','http://localhost:9090/fc')
 
 logger = logging.getLogger(__name__)
@@ -24,21 +25,25 @@ def lookup_in_cdx(qurl, target_date=None):
 
     # Set up default:
     if target_date is None:
-        target_date = datetime.datetime.now()
+        target_dt = datetime.datetime.now()
     else:
-        target_date = datetime.datetime.strptime(target_date, WAYBACK_TS_FORMAT)
+        # Allow ISO date format, or 14-digit format:
+        try:
+            target_dt = datetime.datetime.strptime(target_date, ISO_TS_FORMAT)
+        except:
+            target_dt = datetime.datetime.strptime(target_date, WAYBACK_TS_FORMAT)
 
     # Go through looking for the closest match:
     matched_date = None
     matched_ts = None
     for ts in matches:
         wb_date = datetime.datetime.strptime(ts, WAYBACK_TS_FORMAT)
-        logger.debug("MATCHING: %s %s %s %s" %(matched_date, target_date, wb_date, ts))
-        logger.debug("DELTA:THIS: %i" %(wb_date-target_date).total_seconds())
+        logger.debug("MATCHING: %s %s %s %s" %(matched_date, target_dt, wb_date, ts))
+        logger.debug("DELTA:THIS: %i" %(wb_date-target_dt).total_seconds())
         if matched_date:
-            logger.debug("DELTA:MATCH: %i" % (matched_date-target_date).total_seconds())
-        if matched_date is None or abs((wb_date-target_date).total_seconds()) < \
-                abs((matched_date-target_date).total_seconds()):
+            logger.debug("DELTA:MATCH: %i" % (matched_date-target_dt).total_seconds())
+        if matched_date is None or abs((wb_date-target_dt).total_seconds()) < \
+                abs((matched_date-target_dt).total_seconds()):
             matched_date = wb_date
             matched_ts = ts
             logger.debug("MATCHED: %s %s" %(matched_date, ts))
