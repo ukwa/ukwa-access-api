84b269eddfb9487057e8fb2f359a248a39052d99
Andrew Jackson <Andrew.Jackson@bl.uk>
2020-12-07 19:54:59 +0000

    IIIF integration basically working.

--- api.py
+++ api.py
@@ -58,15 +58,11 @@ class PatchedApi(Api):
             return url_for(self.endpoint('specs'), _external=True)
 
 # Set up the API base:
-api = PatchedApi(app, version='1.0', title=os.environ.get('API_LABEL', 'UKWA API (TEST)'), doc="/doc/",
+api = PatchedApi(app, version='1.0', title=os.environ.get('API_LABEL', 'UKWA API (TEST)'), doc=None,
           description='API services for interacting with UKWA content. \
                       This is an early-stage prototype and may be changed without notice. \
                       TBA: A note about the separate Wayback API? <a href="http://www.mementoweb.org/guide/quick-intro/">Memento</a>')
 app.config.PREFERRED_URL_SCHEME = 'https'
-
-@app.route('/redoc/')
-def redoc():
-    return render_template('redoc.html')
 
 
 class RenderedPageSchema(fields.Raw):
@@ -130,11 +126,9 @@ class CDXServer(Resource):
     @ns.response(200, 'TBA...')
     def get(self):
         """
-        Query our CDX index.
+        Lookup a URL in the UK Web Archive CDX index.
 
-        TBA...
-
-        Note that our Wayback service also supports the Memento API as per https://tools.ietf.org/html/rfc7089
+        Note that our <a href="/wayback/archive/">Wayback service</a> also supports the Memento API as per https://tools.ietf.org/html/rfc7089
 
         """
         # Should open a streaming call to cdx.api.wa.bl.uk/data-heritrix and stream the results back...
@@ -142,133 +136,36 @@ class CDXServer(Resource):
 
 
 # ----------
-# Render API
+# IIIF API
 # ----------
-nsr = api.namespace('Render', path="/render", description='Access to crawl-time and post-crawl screenshots of archived websites.')
-
-@nsr.route('/')
-@nsr.param('url', 'URL to look up.', required=True, location='args', default=EXAMPLE_URL)
-@nsr.param('source', 'The source of the screenshot', enum=['original', 'archive'], required=False, location='args', default='original')
-@nsr.param('type', 'The type of screenshot to retrieve', enum=['thumbnail', 'screenshot', 'har', 'onreadydom', 'imagemap', 'pdf'],
-          required=False, location='args', default='thumbnail')
-@nsr.param('target_date', 'The target date and time to use, as a 14-character (Wayback-style) timestamp (e.g. 20190101120000)', required=False, location='args' )
-class Screenshot(Resource):
-
-    @nsr.doc(id='get_rendered_original', model=RenderedPageSchema)
-    @nsr.produces(['image/*'])
-    @nsr.response(404, 'No screenshot for that url has been captured during crawls.')
-    def get(self):
-        """
-        Grabs an screenshot of an web page.
-
-        This looks for a screenshot of a web page, returning the most recent archived version by default.
-
-        If the <tt>source</tt> is set to <tt>original</tt> the system will \
-        attempt to fine a screenshot of the original web site, as seen at crawl time. If the <tt>source</tt> is set to \
-        <tt>archive</tt> then a rendering of the archived version of the page will be returned instead.
-
-        All seeds should have a <tt>screenshot</tt> - the other rendered types are usually present with the exception of 'pdf' which is under development.
-
-        """
-        url = request.args.get('url')
-        type = request.args.get('type', 'screenshot')
-        source = request.args.get('source', 'original')
-        target_date = request.args.get('target_date', None)
-
-        # First check with a Wayback service to see if this URL is allowed:
-        # This defaults to the public OA service, to avoid accidentally making non-OA material available.
-        r = requests.get("%s%s" %(WAYBACK_SERVER, url))
-        if r.status_code < 200 or r.status_code >= 400:
-            abort(Response(r.reason, status=r.status_code))
-
-        # Check the cache:
-        cache_tag = "%s:%s:%s:%s" % (target_date, source, type, url)
-        result = screenshot_cache.get(cache_tag)
-        if result is not None:
-            #app.logger.info("Found in cache: %s" % qurl)
-            return send_file(io.BytesIO(result['payload']), mimetype=result['content_type'])
-
-        # Query URL
-        qurl = "%s:%s" % (type, url)
-
-        # For originals:
-        if source == 'original':
-            # Query CDX Server for the item
-            (warc_filename, warc_offset, compressed_end_offset) = lookup_in_cdx(qurl, target_date)
-
-            # If not found, say so:
-            if warc_filename is None:
-                abort(404)
-
-            # Grab the payload from the WARC and return it.
-            stream, content_type = get_rendered_original_stream(warc_filename,warc_offset, compressed_end_offset)
-        else:
-            # Get rendered version from internal API
-            r = requests.get(WEBRENDER_ARCHIVE_SERVER,
-                             params={ 'url': url, 'show_screenshot': True, 'target_date': target_date })
-            stream = io.BytesIO(r.content)
-            content_type = "image/png"
-
-        # Get the image:
-        png_data = stream.read()
-
-        # Crop
-        full_jpeg, thumb_jpeg = full_and_thumb_jpegs(png_data, crop=True)
-        content_type = "image/jpeg"
-
-        # Cache and return:
-        screenshot_cache.set(cache_tag, {'payload': full_jpeg, 'content_type': content_type}, timeout=60*60)
-        return send_file(io.BytesIO(full_jpeg), mimetype=content_type)
-
-
-@nsr.route('/list')
-@nsr.param('url', 'URL to look up.', required=True, location='args', default=EXAMPLE_URL)
-@nsr.param('source', 'The source of the screenshot', enum=['original', 'archive'], required=False, location='args', default='original')
-@nsr.param('type', 'The type of screenshot to retrieve', enum=['thumbnail', 'screenshot', 'har', 'onreadydom', 'imagemap', 'pdf'],
-          location='args', required=False, default='thumbnail')
-class Screenshot(Resource):
-
-    @nsr.doc(id='get_screenshot_list')
-    def get(self):
-        """
-        Lists the available crawl-time screenshots
-        """
-        url = request.args.get('url')
-        type = request.args.get('type', 'screenshot')
-        source = request.args.get('source', 'original')
-
-        # First check with a Wayback service to see if this URL is allowed:
-        # This defaults to the public OA service, to avoid accidentally making non-OA material available.
-        r = requests.get("%s%s" %(WAYBACK_SERVER, url))
-        if r.status_code < 200 or r.status_code >= 400:
-            abort(Response(r.reason, status=r.status_code))
-
-        # Query URL
-        if source == 'original':
-            qurl = "%s:%s" % (type, url)
-
-            return jsonify(list_from_cdx(qurl))
-        else:
-            return jsonify(list_from_cdx(url))
-
-
-
-
-@nsr.route('/iiif/2/<string:pwid>/<string:region>/<string:size>/<int:rotation>/<string:quality>.<string:format>')
-@nsr.param('pwid', 'A <a href="">Persistent Web IDentifier (PWID) URN</a>. Must be URL-encoded or Base64 encoded.',
+nsr = api.namespace('IIIF', path="/iiif", description='Access screenshots of archived websites via the <a href="https://iiif.io/api/">IIIF</a> <a href="https://iiif.io/api/image/2.1/">Image API 2.1</a>')
+@nsr.route('/2/<path:pwid>/<string:region>/<string:size>/<int:rotation>/<string:quality>.<string:format>')
+@nsr.param('pwid', 'A <a href="">Persistent Web IDentifier (PWID) URN</a>. Must be twice-URL-encoded or Base64 encoded.',
           required=True, default='urn:pwid:webarchive.org.uk:1995-04-18T15:56:00Z:page:http:%2F%2F%2Fportico.bl.uk%2F')
-@nsr.param('region', 'IIIF image region.', required=True, default='full')
-@nsr.param('size', 'IIIF image size.', required=True, default='full')
-@nsr.param('rotation', 'IIIF image rotation (degrees).', required=True, default='0')
-@nsr.param('quality', 'IIIF image quality.', required=True, default='default', enum=['default', 'grey'])
-@nsr.param('format', 'IIIF image format.', required=True, default='png', enum=['png','jpg'])
+@nsr.param('region', 'IIIF image request <a href="https://iiif.io/api/image/2.1/#region">region</a>.', required=True, default='full')
+@nsr.param('size', 'IIIF image request <a href="https://iiif.io/api/image/2.1/#size">size</a>.', required=True, default='full')
+@nsr.param('rotation', 'IIIF image request <a href="https://iiif.io/api/image/2.1/#rotation">rotation (degrees)</a>.', required=True, default='0')
+@nsr.param('quality', 'IIIF image request <a href="https://iiif.io/api/image/2.1/#quality">quality</a>.', required=True, default='default', enum=['default', 'grey'])
+@nsr.param('format', 'IIIF image request <a href="https://iiif.io/api/image/2.1/#format">format</a>.', required=True, default='png', enum=['png','jpg'])
 class IIIFRenderer(Resource):
-    @ns.doc(id='iiif')
+    @ns.doc(id='iiif_2')
     @ns.response(200, 'The requested image, if available.')
     def get(self, pwid, region, size, rotation, quality, format):
         """
+        Provides images of rendered archived web pages a <a href="https://iiif.io/api/">IIIF</a> <a href="https://iiif.io/api/image/2.1/">Image API 2.1</a>.
         """
-        return redirect('/wayback/archive/%s/%s' % (timestamp, url), code=307)
+
+        # Proxy requests to IIIF server:
+        resp = requests.request(
+            method='GET',
+            url=f"http://iiif:8182/iiif/2/{pwid}/{region}/{size}/{rotation}/{quality}.{format}",
+            headers={key: value for (key, value) in request.headers if key != 'Host'}
+            )
+
+        headers = [(name, value) for (name, value) in resp.raw.headers.items()]
+
+        response = Response(resp.content, resp.status_code, headers)
+        return response
 
 
 
@@ -276,16 +173,6 @@ class IIIFRenderer(Resource):
 # Statistics
 # -------------------------------
 nss = api.namespace('Statistics', path="/stats", description='Information and summary statistics.')
-
-@nss.route('/crawl/recent-screenshots')
-class Screenshots(Resource):
-
-    @ns.doc(id='get_screenshots_dashboard')
-    @ns.produces(['text/html'])
-    def get(self):
-        stats = load_stats()
-        return Response(render_template('screenshots.html', title="Recent Screenshots", stats=stats), mimetype='text/html')
-
 
 @nss.route('/crawl/recent-activity')
 class Crawler(Resource):
@@ -331,8 +218,6 @@ class SaveThisPage(Resource):
 
         Use this to request a URL be saved. If it's in scope for the UK Web Archive, it will be queued for crawling ASAP. If it's out of scope if will be logged for review, to see if we can include in in future.
 
-        Either way, the URL will also be posted to the Internet Archive URL Save This Page service as well.
-
         """
         sr = { 'url': url, 'result': {} }
         # First enqueue for crawl, if configured:
@@ -343,14 +228,81 @@ class SaveThisPage(Resource):
             sr['result']['ukwa'] = {'event': 'save-page-now', 'status': 500, 'reason': e }
 
         # Then also submit request to IA
-        try:
-            ia_save_url = "https://web.archive.org/save/%s" % url
-            r = requests.get(ia_save_url)
-            sr['result']['ia'] = {'event': 'save-page-now',  'status': r.status_code, 'reason': r.reason }
-        except Exception as e:
-            sr['result']['ia'] = {'event': 'save-page-now', 'status': 500, 'reason': e }
+        ## Commenting out for now, as unsure if this is working properly.
+        #try:
+        #    ia_save_url = "https://web.archive.org/save/%s" % url
+        #    r = requests.get(ia_save_url)
+        #    sr['result']['ia'] = {'event': 'save-page-now',  'status': r.status_code, 'reason': r.reason }
+        #except Exception as e:
+        #    sr['result']['ia'] = {'event': 'save-page-now', 'status': 500, 'reason': e }
 
         return jsonify(sr)
+
+
+
+
+# ------------------------------
+# ------------------------------
+# Additional Routes (non-API)
+# ------------------------------
+# ------------------------------
+
+@app.route('/doc/')
+def redoc():
+    return render_template('redoc.html')
+
+@app.route('/render_raw')
+def render_raw():
+    """
+    Grabs an screenshot of an web page.
+
+    This looks for a screenshot of a web page, returning the most recent archived version by default.
+
+    If the <tt>source</tt> is set to <tt>original</tt> the system will \
+    attempt to fine a screenshot of the original web site, as seen at crawl time. If the <tt>source</tt> is set to \
+    <tt>archive</tt> then a rendering of the archived version of the page will be returned instead.
+
+    All seeds should have a <tt>screenshot</tt> - the other rendered types are usually present with the exception of 'pdf' which is under development.
+
+    Caching should be done up-stream.
+
+    """
+    url = request.args.get('url')
+    type = request.args.get('type', 'screenshot')
+    source = request.args.get('source', 'archive')
+    target_date = request.args.get('target_date', None)
+
+    # First check with a Wayback service to see if this URL is allowed:
+    # This defaults to the public OA service, to avoid accidentally making non-OA material available.
+    r = requests.get("%s%s" %(WAYBACK_SERVER, url))
+    if r.status_code < 200 or r.status_code >= 400:
+        abort(Response(r.reason, status=r.status_code))
+
+    # Query URL
+    qurl = "%s:%s" % (type, url)
+
+    # For originals:
+    if source == 'original':
+        # Query CDX Server for the item
+        (warc_filename, warc_offset, compressed_end_offset) = lookup_in_cdx(qurl, target_date)
+
+        # If not found, say so:
+        if warc_filename is None:
+            abort(404)
+
+        # Grab the payload from the WARC and return it.
+        stream, content_type = get_rendered_original_stream(warc_filename,warc_offset, compressed_end_offset)
+    else:
+        # Get rendered version from internal API
+        r = requests.get(WEBRENDER_ARCHIVE_SERVER,
+                            params={ 'url': url, 'show_screenshot': True, 'target_date': target_date })
+        if r.status_code != 200:
+            abort(Response(r.reason, status=r.status_code))
+        stream = io.BytesIO(r.content)
+        content_type = "image/png"
+
+    # And return
+    return send_file(stream, mimetype=content_type)
 
 
 if __name__ == '__main__':
